ef0c6e13bebd98af6ebc27ca123c2cda182c79fa
Nick Robinson <nrob42@gmail.com>
2022-08-24 15:16:43 -0400

    on work computer

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -1,27 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-  const Posts = sequelize.define("Posts", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    postText: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-
-  Posts.associate = (models) => {
-    Posts.hasMany(models.Comments, {
-      onDelete: "cascade",
+    const Posts = sequelize.define("Posts", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      postText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     });
-
-    Posts.hasMany(models.Likes, {
-      onDelete: "cascade",
-    });
+  
+    Posts.associate = (models) => {
+      Posts.hasMany(models.Comments, {
+        onDelete: "cascade",
+      });
+  
+      Posts.hasMany(models.Likes, {
+        onDelete: "cascade",
+      });
+    };
+    return Posts;
   };
-  return Posts;
-};
